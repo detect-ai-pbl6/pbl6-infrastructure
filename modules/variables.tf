0e87ca33551ac6ffe1ef96d741745d745991b0d0
Minh Ngoc Nguyen <minhngocnguyen.403@gmail.com>
2024-10-22 00:11:12 +0700

    feat: set up oidc

--- modules/variables.tf
+++ modules/variables.tf
@@ -43,26 +43,3 @@ variable "secret_key" {
   type        = string
   description = "server secret key"
 }
-
-variable "service_account_roles" {
-  description = "List of roles to assign to the service account"
-  type        = list(string)
-  default = [
-    "roles/run.admin",                      # Ability to manage Cloud Run services
-    "roles/iam.serviceAccountUser",         # Ability to run operations as the service account
-    "roles/iam.serviceAccountTokenCreator", # Ability to run operations as the service account
-    "roles/artifactregistry.writer",        # Ability to push container images
-
-    # Optional roles based on your needs
-    "roles/storage.objectViewer", # If you need to read from GCS
-    "roles/storage.objectAdmin",
-    "roles/storage.admin",
-    "roles/logging.logWriter" # Write application logs
-  ]
-}
-
-variable "github_repo" {
-  description = "GitHub repository in format: OWNER/REPOSITORY"
-  type        = string
-  default     = "detect-ai-pbl6/pbl6-infrastructure"
-}
